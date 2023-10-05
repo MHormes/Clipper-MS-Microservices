f6659d1925fe90913d1aeea487ff3652a96f091f
Maarten Hormes <maarten.hormes@gmail.com>
2023-10-05 10:02:24 +0200

    Walking skeleton setup

---
+++ trading/src/test/java/clipperms/trading/TradingModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package clipperms.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
